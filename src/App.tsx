9c627ad27b93e0e240c86b951856a0db198c85f5
dev-viniciuss <dev.mvsc@gmail.com>
2021-06-25 17:44:02 -0300

    feat: estrutrando a página de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Login } from "./pages/Login"
+
+import './styles/global.scss'
+
 function App() {
   return (
-    <h1>Hello World</h1>
-  );
+    <Login />
+  )
 }
 
-export default App;
+export default App
